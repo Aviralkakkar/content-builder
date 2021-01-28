ebfc4a9c7bd59cb20f8118142619fd97492a3269
Aviral Kakkar <aviralkakkar3@gmil.com>
2021-01-28 17:30:25 +0530

    first commit

--- app.js
+++ app.js
@@ -282,10 +282,9 @@ app.post("/asset", async (req, res) => {
                             }
                         await resCall.write(JSON.stringify({
                         dataToWrite
-                
-                        
+                                   
                     }));
-                    await resCall.write("+");
+            //        await resCall.write("+");
                     console.log("Response Written");
                     //  console.log("Res"+JSON.stringify(resCall));
 
