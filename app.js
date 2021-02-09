1755cf5e3c2b7d53f2742c08a343397c695de485
Aviral Kakkar <aviralkakkar3@gmil.com>
2021-02-09 15:58:23 +0530

    first commit

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ app.post("/call", async (reqCall,resCall)=>
     for (const [keyimage, value] of Object.entries(images)) 
     {
       console.log("yeh hai images ka target data extension" + value.targetName);
-      console.log("This is template Array ----> " + JSON.stringify(value.name));
+      console.log("This is template Array ----> " + JSON.stringify(value.imagejson));
     //  console.log("This is template  ----> " + JSON.stringify(value.id.name));
       console.log("This is template Array ----> " + JSON.stringify(value));
       console.log("This is template  ----> " + value.id.name);
