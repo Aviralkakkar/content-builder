a983444b0724837132c1974b1ffbe71ddb52962d
Aviral Kakkar <aviralkakkar3@gmil.com>
2021-02-17 15:36:46 +0530

    commit

--- app.js
+++ app.js
@@ -304,6 +304,8 @@ app.post("/call", async (reqCall,resCall)=>
   //   console.log("IMAGES  with key " + images[QueryKey].queryText);
   //   console.log("IMAGES" + JSON.stringify(images[QueryKey]));
      console.log("images[myobject].targetName  check --->  " + images[key].targetName);
+     console.log("images descrpition" + images[key].description
+     );
     // console.log("yeh hai images ki key" + images[ResultList[key].Name]);
     // if ( templateIdArray.includes(ResultList[key].Name) == true )
       if ( targetDEArray.includes(images[key].targetName) == true )
