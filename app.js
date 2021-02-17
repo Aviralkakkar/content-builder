052fdce8d1a80b9ecd07dcb628a6163a7fbb258e
Aviral Kakkar <aviralkakkar3@gmil.com>
2021-02-17 12:07:50 +0530

    commit

--- app.js
+++ app.js
@@ -274,13 +274,15 @@ app.post("/call", async (reqCall,resCall)=>
      var targetDEArray = [];
     for (var key in ResultList) 
     {
+
       console.log("yeh hai target data extension" + ResultList[key].Name)   
       targetDEArray.push(ResultList[key].Name);
     // console.log( "yeh hai images me se target de ka name " + images[ResultList[key].Name.targetKey] );
-     
-     console.log( "yeh hai images me se target de ka key" + images[ResultList[key].Name].targetKey );
+     if( images[ResultList[key].Name] != undefined)
+     {
+    // console.log( "yeh hai images me se target de ka key" + images[ResultList[key].Name].targetKey );
      console.log( "yeh hai images me se target de ka name " + JSON.stringify(images[ResultList[key].Name]) );
-
+     }
     }
     console.log( "yo   :      "  + targetDEArray ); 
     for (var key in ResultList) 
